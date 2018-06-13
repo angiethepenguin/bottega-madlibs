f7baaf0606eaa605a60ef1c89157c03afb4bf281
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-06-13 16:04:47 -0600

    small bug fixes

--- src/components/card.js
+++ src/components/card.js
@@ -3,11 +3,33 @@ import React, { Component } from "react";
 import Input from "./input";
 
 class Card extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      color: "",
+      pluralNoun: ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="card">
-        {Input("Color")}
-        {Input("Plural Noun")}
+        <h1>{this.state.color}</h1>
+        {Input("Color", this.state.color, this.handleInputChange, "color")}
+        {Input(
+          "Plural Noun",
+          this.state.pluralNoun,
+          this.handleInputChange,
+          "pluralNoun"
+        )}
       </div>
     );
   }

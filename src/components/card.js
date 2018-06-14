87ce1d118ac83c5199836868ab53a7614cd32af4
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-06-14 13:44:25 -0600

    created Content component to generate story

--- src/components/card.js
+++ src/components/card.js
@@ -7,8 +7,22 @@ class Card extends Component {
     super();
 
     this.state = {
-      color: "",
-      pluralNoun: ""
+      color: '',
+      pluralNoun: '',
+      adjectiveOne: '',
+      celebOne: '',
+      adjectiveTwo: '',
+      nounOne: '',
+      numberOne: '',
+      numberTwo: '',
+      nounTwo: '',
+      adjectiveThree: '',
+      celebTwo: '',
+      celebThree: '',
+      adjectiveFour: '',
+      nounThree: '',
+      celebFour: '',
+      adjectiveFive: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -20,16 +34,37 @@ class Card extends Component {
   }
 
   render() {
+
+    const inputData = [
+      { title: 'color', this.state.color, name: 'color' },
+      { title: 'Plural Noun', this.state.pluralNoun, name: 'pluralNoun' },
+      { title: 'Adjective', this.state.adjectiveOne, name: 'adjectiveOne' },
+      { title: 'Celebrity', this.state.celebOne, name: 'celebOne' },
+
+      { title: 'Adjective', this.state.adjectiveTwo, name: 'adjectiveTwo' },
+      { title: 'Noun', this.state.nounOne, name: 'nounOne' },
+      { title: 'Number', this.state.numberOne, name: 'numberOne' },
+      { title: 'Number', this.state.numberTwo, name: 'numberTwo' },
+
+      { title: 'Noun', this.state.nounTwo, name: 'nounTwo' },
+      { title: 'Adjective', this.state.adjectiveThree, name: 'adjectiveThree' },
+      { title: 'Celebrity', this.state.celebTwo, name: 'celebTwo' },
+      { title: 'Celebrity', this.state.celebThree, name: 'celebThree' },
+
+      { title: 'Adjective', this.state.adjectiveFour, name: 'adjectiveFour' },
+      { title: 'Noun', this.state.nounThree, name: 'nounThree' },
+      { title: 'Celebrity', this.state.celebFour, name: 'celebFour' },
+      { title: 'Adjective', this.state.adjectiveFive, name: 'adjectiveFive' },
+    ]
+
+
     return (
       <div className="card">
-        <h1>{this.state.color}</h1>
-        {Input("Color", this.state.color, this.handleInputChange, "color")}
-        {Input(
-          "Plural Noun",
-          this.state.pluralNoun,
-          this.handleInputChange,
-          "plural Noun"
-        )}
+        {
+          inputData.map(data => Input((data), this.handleInputChange))
+        }
+        <Content />
+
       </div>
     );
   }
